dc5f12f910986b0553a22a951635109d46e43ca3
Mohak327 <mohak327@gmail.com>
2020-07-12 07:22:04 +0530

    m

--- examples/Lecture48/js/script.js
+++ examples/Lecture48/js/script.js
@@ -1,20 +1,28 @@
 // Function constructors
-function Circle (radius) {
+function Circle(radius) {
   this.radius = radius;
 }
 
-Circle.prototype.getArea = 
-  function () {
-    return Math.PI * Math.pow(this.radius, 2);
-  };
-
+Circle.prototype.getArea = function () {
+  return Math.PI * Math.pow(this.radius, 2);
+};
 
 var myCircle = new Circle(10);
-console.log(myCircle.getArea());
+console.log(" Area of circle: " + myCircle.getArea());
 
 var myOtherCircle = new Circle(20);
 console.log(myOtherCircle);
 
+// // Function constructors
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.getArea = function () {
+//     return Math.PI * Math.pow(this.radius, 2);
+//   };
+// }
 
+// var myCircle = new Circle(10);
+// console.log(" Area of circle: " + myCircle.getArea());
 
-
+// var myOtherCircle = new Circle(20);
+// console.log(" Area of other circle: " + myOtherCircle.getArea());
